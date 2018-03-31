06bd2b18c9d6ff20f1542d1264def570079e26d8
Alexis Berry <alexisberry@alexiss-mbp-2.attlocal.net>
2018-03-31 16:50:59 -0400

    add function to headings branch

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$( document ).ready(function() {
+  var headings = $( 'h2' );
+  var headings = $('<h2>I’m Alexis Berry, and I’m learning jQuery!</h2>');
+  $( 'h2' ).after( '<p>I’m Alexis Berry, and I’m learning jQuery!</p>' );
+});
